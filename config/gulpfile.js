624c7e08d2f019818fa57952fe913aace46c2705
maurocappeCG <mauro.cappelloni@cloudgaia.com>
2019-05-07 14:15:20 -0300

    Added gulp zip task

--- config/gulpfile.js
+++ config/gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
+var zip = require('gulp-zip');
 
 gulp.task('less', function() {
     return gulp.src('../force-app/main/default/lwc/**/*.less')  // only compile the entry file
@@ -11,4 +12,11 @@ gulp.task('watch-less', function() {
     gulp.watch('../force-app/main/default/lwc/**/*.less', gulp.series('less'));  // Watch all the .less files into styles, then run the less task
 });
 
-gulp.task('default', gulp.series('watch-less')); // Default will run the 'entry' watch task
+gulp.task('default', gulp.series('watch-less'));
+
+gulp.task('zip', function () {
+    return gulp.src('../front/Assets')
+        .pipe(zip('Assets.zip'))
+        .pipe(gulp.dest('../front'));
+});
+// Default will run the 'entry' watch task
