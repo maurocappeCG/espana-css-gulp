fa70dc3a7e1b6ea509b9a06def9d2dfb63b38728
Tomas Causa <tomas.causa@cloudgaia.com>
2019-05-07 11:58:04 -0300

    Removed salesforce confg files

--- .forceCode/mauro.cappelloni@diageospain.com.dev3/.sfdx/typings/lwc/apex/MD_AccountDetailsController.d.ts
+++
@@ -1,9 +0,0 @@
-declare module "@salesforce/apex/MD_AccountDetailsController.getWrapper" {
-  export default function getWrapper(): Promise<any>;
-}
-declare module "@salesforce/apex/MD_AccountDetailsController.updateContactWithSubscriptions" {
-  export default function updateContactWithSubscriptions(param: {subscriptionValues: any, contactId: any}): Promise<any>;
-}
-declare module "@salesforce/apex/MD_AccountDetailsController.doUpdateUserPassword" {
-  export default function doUpdateUserPassword(param: {newPassword: any}): Promise<any>;
-}
--- .forceCode/mauro.cappelloni@diageospain.com.dev3/.sfdx/typings/lwc/apex/MD_ContactUsHOCController.d.ts
+++
@@ -1,6 +0,0 @@
-declare module "@salesforce/apex/MD_ContactUsHOCController.initialize" {
-  export default function initialize(): Promise<any>;
-}
-declare module "@salesforce/apex/MD_ContactUsHOCController.doSave" {
-  export default function doSave(param: {newCase: any}): Promise<any>;
-}
